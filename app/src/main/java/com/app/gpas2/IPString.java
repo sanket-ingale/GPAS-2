acecd460d0fc10b6ea120ab04491241d81f44e1b
aniketK98 <36024733+aniketK98@users.noreply.github.com>
2020-04-29 19:07:56 +0530

    ipstrings

--- app/src/main/java/com/app/gpas2/IPString.java
+++ app/src/main/java/com/app/gpas2/IPString.java
@@ -1,20 +1,20 @@
 package com.app.gpas2;
 
 public class IPString {
-    public static String ip = "http://192.168.29.229/freshtrop/project/android/scrpit.php";
-    public static String loginString = "http://192.168.29.229/freshtrop/project/android/login.php";
-    public static String loadPersonString = "http://192.168.29.229/freshtrop/project/android/getUser.php";
-    public static String UrlInsert = "http://192.168.29.229/freshtrop/project/android/adduser.php";
-    public static String UrlPass = "http://192.168.29.229/freshtrop/project/android/changePass.php";
-    public static String UrlAddVisit = "http://192.168.29.229/freshtrop/project/android/addVisit.php";
-    public static String UrlButtonScripts = "http://192.168.29.229/freshtrop/project/android/buttonscripts.php";
+//    public static String ip = "http://192.168.29.229/freshtrop/project/android/scrpit.php";
+//    public static String loginString = "http://192.168.29.229/freshtrop/project/android/login.php";
+//    public static String loadPersonString = "http://192.168.29.229/freshtrop/project/android/getUser.php";
+//    public static String UrlInsert = "http://192.168.29.229/freshtrop/project/android/adduser.php";
+//    public static String UrlPass = "http://192.168.29.229/freshtrop/project/android/changePass.php";
+//    public static String UrlAddVisit = "http://192.168.29.229/freshtrop/project/android/addVisit.php";
+//    public static String UrlButtonScripts = "http://192.168.29.229/freshtrop/project/android/buttonscripts.php";
 
-//    public static String ip = "http://freshtrop.000webhostapp.com/android/scrpit.php";
-//    public static String loginString = "http://freshtrop.000webhostapp.com/android/login.php";
-//    public static String loadPersonString = "http://freshtrop.000webhostapp.com/android/getUser.php";
-//    public static String UrlInsert = "http://freshtrop.000webhostapp.com/android/adduser.php";
-//    public static String UrlPass = "http://freshtrop.000webhostapp.com/android/changePass.php";
-//    public static String UrlAddVisit = "http://freshtrop.000webhostapp.com/android/addVisit.php";
-//    public static String UrlButtonScripts = "http://freshtrop.000webhostapp.com/android/buttonscripts.php";
+    public static String ip = "http://freshtrop.000webhostapp.com/project/android/scrpit.php";
+    public static String loginString = "http://freshtrop.000webhostapp.com/project/android/login.php";
+    public static String loadPersonString = "http://freshtrop.000webhostapp.com/project/android/getUser.php";
+    public static String UrlInsert = "http://freshtrop.000webhostapp.com/project/android/adduser.php";
+    public static String UrlPass = "http://freshtrop.000webhostapp.com/project/android/changePass.php";
+    public static String UrlAddVisit = "http://freshtrop.000webhostapp.com/project/android/addVisit.php";
+    public static String UrlButtonScripts = "http://freshtrop.000webhostapp.com/project/android/buttonscripts.php";
 
 }
